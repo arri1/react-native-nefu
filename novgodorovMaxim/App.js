f03dd4d41bc54dd6c39f25819594eb9651e2e7a0
BJladika <BJladika@gmail.com>
2021-02-03 16:39:15 +0900

    BJladika Commit

---
+++ novgodorovMaxim/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Hello BJladika!</Text>
+    </View>
+  );
+};
+
+export default App;
