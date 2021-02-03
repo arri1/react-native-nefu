79b8b4c8ec339ce2bcad6ea64e833c44589441fd
mSE26 <Nikif1996@mail.ru>
2021-02-03 16:48:43 +0900

    First Commit

---
+++ nikiforovIlya/App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+const App= () => {
+  return (
+    <View>
+      <Text>
+        Hola!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
