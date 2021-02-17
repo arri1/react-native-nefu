c25f8488a7b7d6ebf1eaa9f26a382d4f4d804f7b
arri <ewws@mail.ru>
2021-02-17 15:50:14 +0900

    added react navigator

--- gerasimovGerogiy/App.js
+++ gerasimovGerogiy/App.js
@@ -1,47 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import UseStateLab from './screens/useStateLab'
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-    const [color, setColor] = useState('red')
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        setCount(count + 1)
-    }, [color])
-    return (
-        <SafeAreaView
-        >
-            <Text
-                style={{textAlign:'center',marginTop:15}}
-            >
-                Lab 2
-            </Text>
-            <TouchableOpacity
-                style={{
-                    width: '100%',
-                    height: 40,
-                    marginTop:24,
-                    backgroundColor: 'yellow',
-                    justifyContent: 'center',
-                    alignItems:'center'
-                }}
-                title={'Change color'}
-                onPress={() => {
-                    console.log(1)
-                    setColor(color === 'red' ? 'blue' : 'red')
-                }}>
-                <Text>
-                    Change color
-                </Text>
-            </TouchableOpacity>
-            <View
-                style={{marginTop: 30, alignSelf: 'center', backgroundColor: color, width: '100%', height: 100}}
-            />
-            <Text style={{marginTop:24}}>
-                {'change count ' + count}
-            </Text>
-        </SafeAreaView>
-    );
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="useStateLab" component={UseStateLab} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 };
 
 export default App;
