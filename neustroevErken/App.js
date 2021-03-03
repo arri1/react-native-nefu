e0190a122d2299af9d0b023de2ccfb3abc6f06e7
Erken Neustroev <ea.neustroev@stud.s-vfu.ru>
2021-03-03 19:13:51 +0900

    added stylesheet, removed logs, formatted code?

--- neustroevErken/App.js
+++ neustroevErken/App.js
@@ -1,55 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import {
-  View,
   Text,
   TouchableOpacity,
   SafeAreaView,
+  StyleSheet,
 } from 'react-native';
 
 const App = () => {
   const [color, setColor] = useState('grey')
   const [color2, setColor2] = useState('grey')
+  const [color3, setColor3] = useState('grey')
   const [count, setCount] = useState(0)
+  const colorss = ['red','white','blue','green','blue', 'black', 'yellow']
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
   useEffect(() => {
     setCount(count + 1)
-  }, [color, color2])
+  }, [color, color2, color3])
 
+  const styles = StyleSheet.create({
+    box1: {
+      position: 'absolute',
+      top: 55,
+      alignSelf: 'center',
+      width: 180,
+      height: 40,
+      backgroundColor: color
+    },
+    box2: {
+      position: 'absolute',
+      top: 95,
+      alignSelf: 'center',
+      width: 180,
+      height: 40,
+      backgroundColor: color2
+    },
+    box3: {
+      position: 'absolute',
+      top: 135,
+      alignSelf: 'center',
+      width: 180,
+      height: 40,
+      backgroundColor: color3,
+    }
+  });
   return (
     <SafeAreaView>
       <Text>
-        Построй флаг Нидерландии (простой тап меняет цвет верхнего блока, а лонгтап меняет цвет нижнего блока)
-        </Text>
-      <View
-        style={{ marginTop: 30, alignSelf: 'center', backgroundColor: color, width: 135, height: 30 }}
-      />
-      <View
-        style={{ marginTop: 30, alignSelf: 'center', backgroundColor: color2, width: 135, height: 30 }}
-      />
+        Построй флаг России (тапай по блокам) Выполнено {count} нажатий
+      </Text>
+
       <TouchableOpacity
-        style={{
-          width: '80%',
-          height: 50,
-          marginTop: 30,
-          backgroundColor: '#1E90FF',
-          justifyContent: 'center',
-          alignSelf: 'center',
-          alignItems: 'center'
-        }}
+        style={styles.box1}
         onPress={() => {
-          console.log(1)
-          setColor(color === 'red' ? 'blue' : 'red')
-        }}
-        onLongPress={() => {
-          console.log(2)
-          setColor2(color2 === 'red' ? 'blue' : 'red')
+          setColor(colorss[getRandomInt(7)])
         }}>
-        <Text style={{ color: 'white' }}>
-          КНОПКА
-                </Text>
+        <Text style={styles.text}>1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.box2}
+        onPress={() => {
+          setColor2(colorss[getRandomInt(7)])
+        }}>
+        <Text style={styles.text}>2</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.box3}
+        onPress={() => {
+          setColor3(colorss[getRandomInt(7)])
+        }}>
+        <Text style={styles.text}>3</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 }
-
 export default App;
