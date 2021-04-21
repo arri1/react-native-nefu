2eac622384ef095d57b36ae908a8ce40782abb45
Erken Neustroev <ea.neustroev@stud.s-vfu.ru>
2021-04-21 15:45:58 +0900

    added lab4

--- neustroevErken/App.js
+++ neustroevErken/App.js
@@ -1,31 +1,18 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import UseStateLab from './screens/useStateLab';
-import HomeScreen from './screens/homeScreen';
-import TodoList from './screens/todoList'
-
-const Tab = createMaterialBottomTabNavigator();
+//import 'react-native-gesture-handler'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { ApolloProvider } from '@apollo/client'
+import client from './utils/apollo'
+import RootStackNavigator from './navigators/rootStackNavigator'
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen 
-          name="useStateLab" 
-          component={UseStateLab}
-        />
-        <Tab.Screen 
-          name="homeScreen" 
-          component={HomeScreen} 
-        />
-        <Tab.Screen 
-          name="todoList" 
-          component={TodoList} 
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <RootStackNavigator />
+      </NavigationContainer>
+    </ApolloProvider>
   )
-};
+}
+
 export default App;
