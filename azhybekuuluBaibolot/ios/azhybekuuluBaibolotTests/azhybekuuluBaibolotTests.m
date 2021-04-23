4409c1491e8c5e9d574f095ac8bc9435d6de85d2
baibolot <azhybekuulu96@bk.ru>
2021-04-23 19:09:23 +0900

    frist lab

--- azhybekuuluBaibolot/ios/azhybekuuluBaibolotTests/azhybekuuluBaibolotTests.m
+++ azhybekuuluBaibolot/ios/azhybekuuluBaibolotTests/azhybekuuluBaibolotTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AzhybekuuluBaibolotTests : XCTestCase
+@interface azhybekuuluBaibolotTests : XCTestCase
 
 @end
 
-@implementation AzhybekuuluBaibolotTests
+@implementation azhybekuuluBaibolotTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
