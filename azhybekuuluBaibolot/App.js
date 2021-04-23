8f5e9590b991db242f275f35984818f72cee2341
baibolot <azhybekuulu96@bk.ru>
2021-04-23 19:21:35 +0900

    second lab

--- azhybekuuluBaibolot/App.js
+++ azhybekuuluBaibolot/App.js
@@ -4,17 +4,28 @@ import {Dimensions, StyleSheet, Text, View, TouchableOpacity, Button
 
 const { height, width } = Dimensions.get('window')
 
+const randomHex = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
 const App = (props) => {
-    const [backgroundColor, setColor] = useState()
+    const [backgroundColor, setColor] = useState(randomHex())
+    const onClick = () => setColor(randomHex())
     return (
         <View style={styles.mainContainer}>
             <TouchableOpacity
+                onPress={onClick}
                 style={[
                     styles.container,
-                    { backgroundColor: "white" }
+                    { backgroundColor: randomHex() }
                 ]}
             >
-                <Text style={styles.instructions}>HELLOW WORLD!</Text>
+                <Text style={styles.instructions}>Замена цветов в один клик!</Text>
             </TouchableOpacity>
         </View >
         
@@ -27,6 +38,7 @@ const styles = StyleSheet.create({
         width,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: randomHex(),
     },
     instructions: {
         color: "black",
